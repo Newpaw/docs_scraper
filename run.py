249b0d450da63c0a91cbc4a616da6b266f2caf1d
Jan Novopacky <jan.novopacky@gmail.com>
2023-07-28 13:06:57 +0200

    add readme and arg

--- run.py
+++ run.py
@@ -1,15 +1,17 @@
 import logging
-import pdfkit
-import time
+import argparse
 import os
+import time
+import uuid
+from typing import Dict, List
 
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
-from typing import List
+import pdfkit
 
 
 logging.basicConfig(
@@ -57,13 +59,13 @@ def setup_webdriver(base_url: str) -> webdriver.Chrome:
 def get_links(driver: webdriver.Chrome, base_url: str, limit: int = None) -> List[str]:
     """
     Gather all unique links from a website that meet certain conditions.
-    
+
     This function uses a Selenium WebDriver instance to navigate a website and collect
     unique links. It first collects links from all 'nav' elements on the base_url page.
     Then, it navigates to each of these links and gathers further links.
     The function can be limited to a certain number of links.
     Logs the number of links found every 15 seconds.
-    
+
     Parameters
     ----------
     driver : webdriver.Chrome
@@ -135,7 +137,34 @@ def get_links(driver: webdriver.Chrome, base_url: str, limit: int = None) -> Lis
     return list(links)[:limit] if limit is not None else list(links)
 
 
-def scrape_page(driver: webdriver.Chrome, url: str) -> dict:
+def scrape_page(driver: webdriver.Chrome, url: str) -> List[Dict[str, str]]:
+    """
+    Navigates to the given URL using the provided Selenium webdriver and
+    scrapes the page content. The function specifically targets the main content
+    of the page using the 'main' HTML tag.
+
+    Parameters
+    ----------
+    driver : webdriver.Chrome
+        The Selenium webdriver used for webpage navigation and content scraping.
+    url : str
+        The URL of the webpage to be scraped.
+
+    Returns
+    -------
+    Dict[str, str]
+        A dictionary containing the URL and the scraped content from the webpage.
+        The dictionary has the following structure:
+        {
+            "url": url of the webpage,
+            "content": scraped content from the webpage
+        }
+
+    Example
+    -------
+    >>> scrape_page(driver, "https://www.example.com")
+    {"url": "https://www.example.com", "content": "This is an example webpage."}
+    """
     logging.debug(f"Scraping URL: {url}")
     driver.get(url)
     WebDriverWait(driver, 10).until(
@@ -147,14 +176,14 @@ def scrape_page(driver: webdriver.Chrome, url: str) -> dict:
     return data
 
 
-def create_pdf(data: List[dict]) -> str:
+def create_pdf_and_get_path(data: List[Dict[str, str]]) -> str:
     """
     Creates a PDF document from the given data. The PDF file is named using
-    the current timestamp to ensure uniqueness.
+    a UUID to ensure uniqueness.
 
     Parameters
     ----------
-    data : List[dict]
+    data : List[Dict[str, str]]
         List of dictionaries containing URL and content.
 
     Returns
@@ -162,9 +191,8 @@ def create_pdf(data: List[dict]) -> str:
     str
         The absolute path of the created PDF document.
     """
-    timestamp = time.strftime("%Y%m%d-%H%M%S")
-    unique_file_name = f"scraped_data_{timestamp}.pdf"
-    
+    unique_file_name = f"scraped_data_{uuid.uuid4()}.pdf"
+
     html_data = '<html><head><meta charset="UTF-8"></head><body>'
     for item in data:
         html_data += "<h1>URL: {}</h1>".format(item["url"])
@@ -174,28 +202,58 @@ def create_pdf(data: List[dict]) -> str:
     output_path = os.path.join(os.getcwd(), unique_file_name)
     pdfkit.from_string(html_data, output_path)
 
-    logging.info(f"PDF file created at {output_path}.")
+    logging.info(f"PDF file created with name {unique_file_name}.pdf.")
 
     return output_path
 
 
-def main():
-    base_url:url = "https://portainer.gitbook.io/"
-    limit_of_pages:int = 2
+def main_scraper(base_url: str, limit_of_pages: int) -> str:
+    """
+    Main function to scrape the webpages at the given base URL and create a PDF document with the scraped data.
 
+    The function initializes a Selenium webdriver, collects the links to be visited from the base URL, 
+    scrapes the pages at the collected URLs, and then creates a PDF document with the scraped data.
+    The PDF document is named with a unique identifier and the function returns the absolute path to the created PDF.
+
+    Parameters
+    ----------
+    base_url : str, optional
+        The base URL from where the links will be collected for scraping, by default "https://docs.mluvii.com/"
+    limit_of_pages : int, optional
+        The maximum number of pages to be scraped, by default 2
+
+    Returns
+    -------
+    str
+        The absolute path to the created PDF document.
+
+    Example
+    -------
+    >>> main_scraper("https://www.example.com", 5)
+    "/home/user/Documents/scraped_data_12345678.pdf"
+    """
     driver = setup_webdriver(base_url)
     urls_to_visit = get_links(driver, base_url, limit=limit_of_pages)
 
     data = []
     number_of_pages = len(urls_to_visit)
-    
+
     for i, url in enumerate(urls_to_visit):
         data.append(scrape_page(driver, url))
         logging.info(f"Scraped {i+1} from {number_of_pages} pages.")
 
     driver.quit()
-    create_pdf(data)
+    path_to_pdf = create_pdf_and_get_path(data)
+    return path_to_pdf
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='Web scraping program.')
+    parser.add_argument('--base_url', type=str, default='https://docs.mluvii.com/', 
+                        help='The base URL from where the links will be collected for scraping.')
+    parser.add_argument('--limit_of_pages', type=int, default=2, 
+                        help='The maximum number of pages to be scraped.')
+
+    args = parser.parse_args()
+
+    main_scraper(args.base_url, args.limit_of_pages)
